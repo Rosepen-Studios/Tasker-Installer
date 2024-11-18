7ed9ceeb99b0a8e60bbc13d4f25bd44292e1e1e5
Firepixel85 <maxdimi2009@gmail.com>
2024-11-18 20:50:57 +0200

    Added Windows support

--- 1/1.gd
+++ 1/1.gd
@@ -1,49 +1,90 @@
 extends Control
 
-
+var os = "WIN"
 @onready var web: HTTPRequest = $HTTPRequest
 @onready var next: Button = $MarginContainer/VBoxContainer2/Control/HBoxContainer/Next
 @onready var title: Label = $MarginContainer/VBoxContainer/HBoxContainer/Label
 @onready var text: Label = $MarginContainer/VBoxContainer/Label
 @onready var timer: Timer = $Timer
 @onready var exit: Button = $MarginContainer/VBoxContainer2/Control/HBoxContainer2/Exit
-var user
+var release_link:String = "https://github.com/Firepixel85/Tasker-Labs/releases/download/obd_1.0/Tasker."
+var user 
 
 func _ready() -> void:
 	user = OS.get_user_data_dir().split("/")[2]
 	next.grab_focus()
 	
 func _on_next_pressed() -> void:
-	next.visible = false
-	exit.visible = false
-	title.text = "Downloading"
-	text.text = "Removing past installations"
-	var output = []
-	var term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm -rf /Applications/Tasker.app"],output)
-	
-	timer.start()
-	await timer.timeout
-	text.text = "Getting file from remote server             (This might take some time)"
-	web.set_download_file("user://Tasker.zip")
-	web.request("https://github.com/Rosepen-Studios/Tasker/releases/download/a0.3.1/Tasker_v0.3.1_Mac.zip")
-	
-	await web.request_completed
-	text.text = "File retrived, expanding ZIP"
-	
-	term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && unzip '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker Installer/Tasker.zip' -d  '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker Installer'"],output)
-	timer.start()
-	await timer.timeout
-	text.text = "Moving app to Applications folder"
-	
-	term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && mv '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker Installer/Tasker.app' /Applications"],output)
-	timer.start()
-	await timer.timeout
-	term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker Installer/Tasker.zip'"],output)
-	text.text = "Deleting ZIP file"
-	timer.start()
-	await timer.timeout
+	if os == "MAC":
+		next.visible = false
+		exit.visible = false
+		title.text = "Downloading"
+		text.text = "Removing past installations"
+		var output = []
+		var term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm -rf /Applications/Tasker.app"],output)
+		
+		timer.start()
+		await timer.timeout
+		text.text = "Getting file from remote server             (This might take some time)"
+		web.set_download_file("user://Tasker.zip")
+		web.request(release_link+"Mac.zip")
+		
+		await web.request_completed
+		text.text = "File retrived, expanding ZIP"
+		
+		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && unzip '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip' -d  '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer'"],output)
+		timer.start()
+		await timer.timeout
+		text.text = "Moving app to Applications folder"
+		
+		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && mv '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.app' /Applications"],output)
+		timer.start()
+		await timer.timeout
+		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip'"],output)
+		text.text = "Deleting ZIP file"
+		timer.start()
+		await timer.timeout
 
-	get_tree().change_scene_to_file("res://2/2.tscn")
+		get_tree().change_scene_to_file("res://2/2.tscn")
+	elif os == "WIN":
+		next.visible = false
+		exit.visible = false
+		title.text = "Downloading"
+		var output = []
+		var term
+		
+		text.text = "Getting file from remote server             (This might take some time)"
+		web.set_download_file("user://Tasker.zip")
+		web.request(release_link+"Windows.zip")
+		
+		await web.request_completed
+		text.text = "Removing past installations"
+		
+		term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path \"C:\\Tasker\\Tasker.exe\""],output)
+		timer.start()
+		await timer.timeout
+		text.text = "File retrived, expanding ZIP"
+
+
+		term = OS.execute("POWERSHELL.exe", ["tar -xf C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip -C C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer"],output)
+		timer.start()
+		await timer.timeout
+		
+		term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip"],output)
+		text.text = "Deleting ZIP file"
+		timer.start()
+		await timer.timeout
+		
+		term = OS.execute("POWERSHELL.exe", ["New-Item -Path \"C:\\Tasker\" -ItemType Directory"],output)
+		text.text = "Creating storage directory"
+		timer.start()
+		await timer.timeout
+		
+		output = []
+		term = OS.execute("POWERSHELL.exe", [ "Move-Item -Path \"C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.exe\"  -Destination \"C:\\Tasker\""],output)
+		text.text = "Moving extracted file"
+		get_tree().change_scene_to_file("res://2/2.tscn")
+
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
--- 2.gd
+++ 2.gd
@@ -2,12 +2,17 @@ extends Control
 
 @onready var next: Button = $MarginContainer/VBoxContainer2/Control/HBoxContainer/Next
 @onready var tick: CheckBox = $MarginContainer/VBoxContainer/CheckBox
-
+var os ="WIN"
 func _ready() -> void:
 	next.grab_focus()
 
 func _on_next_pressed() -> void:
-	if tick.button_pressed == true:
-		OS.shell_open("/Applications/Tasker.app")
-	get_tree().quit()
+	if os == "MAC":
+		if tick.button_pressed == true:
+			OS.shell_open("/Applications/Tasker.app")
+		get_tree().quit()
+	elif  os == "WIN":
+		if tick.button_pressed == true:
+			OS.shell_open("C:\\Tasker\\Tasker.exe")
+		get_tree().quit()
 	

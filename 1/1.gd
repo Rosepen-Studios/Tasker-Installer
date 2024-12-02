d535e49801c8ffd07c22ae829b55a9f3d1870039
_M2x <maxdimi2009@gmail.com>
2024-12-02 17:42:41 +0200

    Added error detection

--- 1/1.gd
+++ 1/1.gd
@@ -17,84 +17,116 @@ func _ready() -> void:
 	next.grab_focus()
 	
 func _on_next_pressed() -> void:
-	load.visible = true
-	loop.play("Load")
-	if os == "MAC":
-		next.visible = false
-		exit.visible = false
-		title.text = "Downloading"
-		text.text = "Removing past installations"
-		var output = []
-		var term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm -rf /Applications/Tasker.app"],output)
+	if next.text == "Install":
+		load.visible = true
+		loop.play("Load")
+		if os == "MAC":
+			next.visible = false
+			exit.visible = false
+			title.text = "Downloading"
+			text.text = "Removing past installations"
+			var output = []
+			var term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm -rf /Applications/Tasker.app"],output)
 
-		timer.start()
-		await timer.timeout
-		text.text = "Getting file from remote server             (This might take some time)"
-		web.set_download_file("user://Tasker.zip")
-		web.request(release_link+"Mac.zip")
-		
-		await web.request_completed
-		text.text = "File retrieved, expanding ZIP"
+			timer.start()
+			await timer.timeout
+			print(output)
+			text.text = "Getting file from remote server             (This might take some time)"
+			web.set_download_file("user://Tasker.zip")
+			web.request(release_link+"Mac.zip")
+			
+			await web.request_completed
+			text.text = "File retrieved, expanding ZIP"
 
-		output = []
-		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && unzip '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip' -d  '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer'"],output)
-		timer.start()
-		await timer.timeout
-		print(output)
-		text.text = "Moving app to Applications folder"
+			output = []
+			term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && unzip '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip' -d  '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer'"],output)
+			timer.start()
+			await timer.timeout
+			for i in output.size():
+				print(output[i])
+			text.text = "Moving app to Applications folder"
 
-		output = []
-		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && mv '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.app' /Applications"],output)
-		timer.start()
-		await timer.timeout
-		print(output)
-		output = []
-		term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip'"],output)
-		text.text = "Deleting ZIP file"
-		timer.start()
-		await timer.timeout
-		print(output)
+			output = []
+			term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && mv '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.app' /Applications"],output)
+			timer.start()
+			await timer.timeout
+			print(output)
+			output = []
+			term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && rm '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip'"],output)
+			text.text = "Deleting ZIP file"
+			timer.start()
+			await timer.timeout
+			print(output)
+			
+			output = []
+			term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && ls /Applications/Tasker.app"],output)
+			if output != [""]:
+				get_tree().change_scene_to_file("res://2/2.tscn")
+			else:
+				var output1 =[]
+				var output2 =[]
+				term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && ls '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.app'"],output1)
+				term = OS.execute("/bin/bash",["-c"]+["cd .. && cd .. && cd .. && cd .. && cd .. && ls '/Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip'"],output2)
+				if output2 != [""]:
+					print("ERROR: Failed to extract zip")
+					text.text= "Failed to extract the zip file located at: /Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.zip"
+					
 
-		get_tree().change_scene_to_file("res://2/2.tscn")
-	elif os == "WIN":
-		next.visible = false
-		exit.visible = false
-		title.text = "Downloading"
-		var output = []
-		var term
-		
-		text.text = "Getting file from remote server             (This might take some time)"
-		web.set_download_file("user://Tasker.zip")
-		web.request(release_link+"Windows.zip")
-		
-		await web.request_completed
-		text.text = "Removing past installations"
-		
-		term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path \"C:\\Tasker\\Tasker.exe\""],output)
-		timer.start()
-		await timer.timeout
-		text.text = "File retrieved, expanding ZIP"
+				elif output1 != [""]:
+					print("ERROR: Failed to move file")
+					text.text= "Failed to move Tasker, app located at: /Users/"+user+"/Library/Application Support/Godot/app_userdata/Tasker_Installer/Tasker.app"
+					
+				else:
+					text.text= "An unknown error ocuured with the instalation, we recomend installing Tasker manualy."
+				
+				exit.visible = true
+				title.text = "Error"
+				next.visible = true
+				load.visible = false
+				next.text = "Manual Install"
+			#ls: /Applications/Tasker.app: No such file or directory
+
+			
+		elif os == "WIN":
+			next.visible = false
+			exit.visible = false
+			title.text = "Downloading"
+			var output = []
+			var term
+			
+			text.text = "Getting file from remote server             (This might take some time)"
+			web.set_download_file("user://Tasker.zip")
+			web.request(release_link+"Windows.zip")
+			
+			await web.request_completed
+			text.text = "Removing past installations"
+			
+			term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path \"C:\\Tasker\\Tasker.exe\""],output)
+			timer.start()
+			await timer.timeout
+			text.text = "File retrieved, expanding ZIP"
 
 
-		term = OS.execute("POWERSHELL.exe", ["tar -xf C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip -C C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer"],output)
-		timer.start()
-		await timer.timeout
-		
-		term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip"],output)
-		text.text = "Deleting ZIP file"
-		timer.start()
-		await timer.timeout
-		
-		term = OS.execute("POWERSHELL.exe", ["New-Item -Path \"C:\\Tasker\" -ItemType Directory"],output)
-		text.text = "Creating storage directory"
-		timer.start()
-		await timer.timeout
-		
-		output = []
-		term = OS.execute("POWERSHELL.exe", [ "Move-Item -Path \"C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.exe\"  -Destination \"C:\\Tasker\""],output)
-		text.text = "Moving extracted file"
-		get_tree().change_scene_to_file("res://2/2.tscn")
-
+			term = OS.execute("POWERSHELL.exe", ["tar -xf C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip -C C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer"],output)
+			timer.start()
+			await timer.timeout
+			
+			term = OS.execute("POWERSHELL.exe", ["Remove-Item -Path C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.zip"],output)
+			text.text = "Deleting ZIP file"
+			timer.start()
+			await timer.timeout
+			
+			term = OS.execute("POWERSHELL.exe", ["New-Item -Path \"C:\\Tasker\" -ItemType Directory"],output)
+			text.text = "Creating storage directory"
+			timer.start()
+			await timer.timeout
+			
+			output = []
+			term = OS.execute("POWERSHELL.exe", [ "Move-Item -Path \"C:\\Users\\"+user+"\\AppData\\Roaming\\Godot\\app_userdata\\Tasker_Installer\\Tasker.exe\"  -Destination \"C:\\Tasker\""],output)
+			text.text = "Moving extracted file"
+			get_tree().change_scene_to_file("res://2/2.tscn")
+	else:
+		OS.shell_open("https://taskerapp.framer.website/manual-installation")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
